00a638c4415e44bbcd4ec422050bca788fd0bf6e
amelaur <amybauerdance@gmail.com>
2024-12-24 14:37:08 -0700

    testing js update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
 }
 
 function searchCity(city) {
@@ -12,8 +14,6 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
