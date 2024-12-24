004808f023c177551da88649cdc93d4edd48bcc8
amelaur <amybauerdance@gmail.com>
2024-12-24 14:40:04 -0700

    updated js to make more sense upon page load

--- src/index.js
+++ src/index.js
@@ -19,3 +19,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Berlin");
