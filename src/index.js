8ebc67b360fc7e5cbba181b9675e92063865424b
amelaur <amybauerdance@gmail.com>
2024-12-24 14:24:40 -0700

    added api functionality, testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "dtacb933eof95d0694704c53009f47c5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
