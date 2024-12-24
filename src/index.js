07f492546a5182079849e055ac7537dc40c2828d
amelaur <amybauerdance@gmail.com>
2024-12-24 14:05:33 -0700

    attempt to fix javascript

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-function handleSearchSubmit(event) {
+function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchFormElement.addEventListener("submit", searchSubmit);
