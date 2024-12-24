83eb6ecc37d895e2b6d072f8d3d733320bfb5938
amelaur <amybauerdance@gmail.com>
2024-12-24 14:04:34 -0700

    re-test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   console.log(searchInput.value);
 }
+
+let searchFormElement = document.querySelector("search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
