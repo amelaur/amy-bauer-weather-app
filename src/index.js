671173345065748d460a407819469bab2774a931
amelaur <amybauerdance@gmail.com>
2025-01-06 15:57:53 -0700

    completed api integration

--- src/index.js
+++ src/index.js
@@ -41,9 +41,16 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "dtacb933eof95d0694704c53009f47c5";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
 
@@ -51,18 +58,24 @@ function displayForecast(response) {
   console.log(response.data);
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
           <div class="weather-forecast-day">
-            <div class="weather-forecast-date">Mon</div>
-            <div class="weather-forecast-icon">⛅️</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div><img src="${
+              day.condition.icon_url
+            }"  class="weather-forecast-icon"/></div>
             <div class="weather-forecast-temperatures">
-              <strong>${day.temperature.maximum}°</strong> ${day.temperature.minimum}°
+              <strong>${Math.round(
+                day.temperature.maximum
+              )}°</strong> /  ${Math.round(day.temperature.minimum)}°
             </div>
           </div>
         `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
